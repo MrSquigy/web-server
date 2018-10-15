0bb9350f55b3dc8fedc578de49e26489b330bfaf
Jonathan Vice <jajvice@gmail.com>
2018-10-15 17:35:25 +0200

    Add a base case class that handles files instead of main handler class. Add cgi functionality

--- cases.py
+++ cases.py
@@ -1,6 +1,26 @@
 import os
 
-class case_no_file(object):
+class base_case(object):
+    # Parent class for case handlers
+    
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            handler.handle_error(msg)
+    
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+    
+    def test(self, handler):
+        assert False, 'Not implemented.'
+    
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+class case_no_file(base_case):
     # File or dir doesn't exist
 
     def test(self, handler):
@@ -9,31 +29,26 @@ class case_no_file(object):
     def act(self, handler):
         raise Exception("'%s' not found" % handler.path)
 
-class case_existing_file(object):
+class case_existing_file(base_case):
     # File exists
+
     def test(self, handler):
         return os.path.isfile(handler.full_path)
 
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
-class case_directory_index_file(object):
+class case_directory_index_file(base_case):
     # Serve index.html page for dir
-
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
     
     def test(self, handler):
         return os.path.isdir(handler.full_path) and os.path.isfile(self.index_path(handler))
     
     def act(self, handler):
-        handler.handle_file(self.index_path(handler))
+        self.handle_file(handler, self.index_path(handler))
 
-class case_directory_no_index_file(object):
+class case_directory_no_index_file(base_case):
     # Serve listing for dir without index page
-    
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
 
     def test(self, handler):
         return os.path.isdir(handler.full_path) and not os.path.isfile(self.index_path(handler))
@@ -41,7 +56,16 @@ class case_directory_no_index_file(object):
     def act(self, handler):
         handler.list_dir(handler.full_path)
 
-class case_always_fail(object):
+class case_cgi_file(base_case):
+    # Something runnable
+
+    def test(self, handler):
+        return os.path.isfile(handler.full_path) and handler.full_path.endswith('.py')
+    
+    def act(self, handler):
+        handler.run_cgi(handler.full_path)
+
+class case_always_fail(base_case):
     # Base case if nothing else worked
 
     def test(self, handler):
--- server.py
+++ server.py
@@ -1,17 +1,17 @@
 import os
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from subprocess import Popen, PIPE
 
 from cases import *
 
-IP = '127.0.0.1'
 PORT = 80
 
 class RequestHandler(BaseHTTPRequestHandler):
 
     error_page = "<html>\n<body>\n<h1>Error accessing {path}</h1>\n<p>{msg}</p>\n</body>\n</html>"
     listing_page = "<html>\n<body>\n<ul>\n{0}\n</ul>\n</body>\n</html>"
-    Cases = [case_no_file(), case_existing_file(), case_directory_index_file(), case_directory_no_index_file(), case_always_fail()]
+    Cases = [case_no_file(), case_cgi_file(), case_existing_file(), case_directory_index_file(), case_directory_no_index_file(), case_always_fail()]
 
     def do_GET(self):
         try:
@@ -23,19 +23,18 @@ class RequestHandler(BaseHTTPRequestHandler):
                     break
         except Exception as msg:
             self.handle_error(msg)
-    
-    def handle_file(self, full_path):
-        try:
-            with open(full_path, 'rb') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            self.handle_error(msg)
 
     def handle_error(self, msg):
         content = self.error_page.format(path = self.path, msg = msg).encode()
         self.send_content(content, 404)
     
+    def send_content(self, content, status = 200):
+        self.send_response(status)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
+
     def list_dir(self, full_path):
         try:
             entries = os.listdir(full_path)
@@ -45,16 +44,16 @@ class RequestHandler(BaseHTTPRequestHandler):
         except OSError as msg:
             msg = "'%s' cannot be listed: %s" %(self.path, msg)
             self.handle_error(msg)
-    
-    def send_content(self, content, status = 200):
-        self.send_response(status)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(content)))
-        self.end_headers()
-        self.wfile.write(content)
+
+    def run_cgi(self, full_path):
+        cmd = "python " + full_path
+        with Popen(cmd, stdout = PIPE) as proc:
+            data = proc.stdout.read()
+
+        self.send_content(data)
 
 if __name__ == '__main__':
-    serverAddress = (IP, PORT)
+    serverAddress = ('', PORT)
     server = HTTPServer(serverAddress, RequestHandler)
 
     print(time.asctime(), "Server Started")
